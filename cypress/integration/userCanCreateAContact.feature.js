ee9198e15960105c3c3044136b0af975f075b2a3
japh <jenny.scherr@svenskaspel.se>
2020-03-28 14:23:20 +0100

    test for all the input field

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,6 +2,11 @@ describe('user can create a contact', () => {
 	it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
-
+		cy.get('#name').type('Jenny')
+		cy.get('#email').type('jenny@scherr.se')
+		cy.get('#phone').type('0767 687974')
+		cy.get('#company').type('Performance IT')
+		cy.get('#notes').type('Performance Engineer')
+		cy.get('#twitter').type('@jennyscherr')
 	})
 })
