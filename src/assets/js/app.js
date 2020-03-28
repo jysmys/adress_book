06417ef5ac10215421fd6ce9bab1283ea353c1b4
japh <jenny.scherr@svenskaspel.se>
2020-03-28 14:55:31 +0100

    added 2 tests for the table

---
+++ src/assets/js/app.js
@@ -0,0 +1,8 @@
+const getInputvalue = () => {
+    let getName = document.getElementById("name").value
+    let getEmail = document.getElementById("email").value
+    let getPhone = document.getElementById("phone").value
+    let getCompany = document.getElementById("company").value
+    let getNotes = document.getElementById("notes").value
+    let getTwitter = document.getElementById("twitter").value
+}
