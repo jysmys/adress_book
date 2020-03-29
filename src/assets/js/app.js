e96ae31ee0335a507f78ac64e4fff3fa0b7189b5
japh <jenny.scherr@svenskaspel.se>
2020-03-29 15:00:27 +0200

    css changes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,31 +1,63 @@
 const  storage = window.localStorage
 
+const tableHeader = table => {
+    let thead = document.createElement('tr')
+    thead.setAttribute('id', 'thead')
+    thead.innerHTML = `
+    <th>Your Contacts</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>`
+    table.appendChild(thead)
+
+    let head = document.createElement('tr')
+    head.setAttribute('id', 'head')
+    head.innerHTML = `
+    <th>Name</th>
+    <th>Email</th>
+    <th>Phone</th>
+    <th>Company</th>
+    <th>Notes</th>`
+    table.appendChild(head)
+}
+const tableEnd = table => {
+    let tend = document.createElement('tr')
+    tend.setAttribute('id', 'tend')
+    tend.innerHTML = `<td></td><td></td><td></td><td></td><td></td>`
+    table.appendChild(tend)
+}
+// const isEmpty = contact =>{
+//     return contact.every((k,v) => v === "")
+//   }
+
 const  renderContacts = () => {
   const  contacts = JSON.parse(storage.getItem('contacts'))
-
   let table = document.querySelector('#contact-list')
+  table.innerHTML = ''
+  tableHeader(table)
+
   if (contacts) {
-	table.innerHTML = ''
-	// document.createElement('table')
-    
 	contacts.forEach(contact  => {
         let  tr = document.createElement('tr')
-		// let  td = document.createElement('td')
+        tr.setAttribute('id', 'body')
 
-		tr.innerHTML = `
-		  <td><p>${contact.name}</p></td>
-		  <td><p>${contact.email}</p></td>
-		  <td><p>${contact.phone}</p></td>
-		  <td><p>${contact.company}</p></td>
-		  <td><p>${contact.notes}</p></td>
-	    `
-	    table.appendChild(tr)
-	  })
-			
-	//   table.appendChild(tr)
+        tr.innerHTML = `
+        <td><p>${contact.name}</p></td>
+        <td><p>${contact.email}</p></td>
+        <td><p>${contact.phone}</p></td>
+        <td><p>${contact.company}</p></td>
+        <td><p>${contact.notes}</p></td>
+        <td><p><i class="glyphicon glyphicon-trash"></i></p></td>
+        `
+        table.appendChild(tr)
+        // console.log(tr.outerHTML)
+        }
+    )
 	} else {
-        table.innerHTML = '<p>You have no contacts in your address book</p>'
-	}
+        table.innerHTML = '<tr><td><p>You have no contacts in your address book</p></td></tr>'
+    }
+    tableEnd(table)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
